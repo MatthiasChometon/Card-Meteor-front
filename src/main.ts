e183708116dad85ca3e9b2de10a71aad4932ee7f
MatthiasChometon <matthiaschometon787@gmail.com>
2022-04-12 13:46:36 +0100

    feat: add Notify plugin to quasar

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { Quasar } from 'quasar'
+import { Notify, Quasar } from 'quasar'
 import App from './App.vue'
 
 import '@quasar/extras/material-icons/material-icons.css'
@@ -19,7 +19,7 @@ export const createApp = ViteSSG(
   { routes, base: import.meta.env.BASE_URL },
   (ctx) => {
     ctx.app.use(Quasar, {
-      plugins: {}, // import Quasar plugins and add here
+      plugins: { Notify },
     })
     // install all modules under `modules/`
     Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
