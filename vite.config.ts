d6ee9f771741b0698a3e332b91fc1e058156b5ed
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-02 17:13:31 +0100

    build: delete Unocss

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import Inspect from 'vite-plugin-inspect'
 import Prism from 'markdown-it-prism'
 import LinkAttributes from 'markdown-it-link-attributes'
-import Unocss from 'unocss/vite'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 const markdownWrapperClasses = 'prose prose-sm m-auto text-left'
@@ -60,10 +59,6 @@ export default defineConfig({
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: 'src/components.d.ts',
     }),
-
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss(),
 
     // https://github.com/antfu/vite-plugin-md
     // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
