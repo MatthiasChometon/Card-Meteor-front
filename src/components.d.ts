7c80d6ff9b713201f1d84c9c8c5305f05e7a082c
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-02 17:58:35 +0100

    feat: update auto import

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,7 +6,7 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Counter: typeof import('./components/Counter.vue')['default']
     Footer: typeof import('./components/Footer.vue')['default']
-    README: typeof import('./components/README.md')['default']
+    Form: typeof import('./components/Form.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
