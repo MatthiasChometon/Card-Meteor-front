6553a6fd3e7e400090fd8f4e9f6bd0cbd6874903
Ruizlouka <ruizlouka981@gmail.com>
2022-05-03 16:03:08 +0200

    feat: add form connexion"

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,6 +6,8 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Footer: typeof import('./components/Footer.vue')['default']
     FormCard: typeof import('./components/FormCard.vue')['default']
+    FormConnexio: typeof import('./components/FormConnexio.vue')['default']
+    FormLogin: typeof import('./components/FormLogin.vue')['default']
     HistoriqueUser: typeof import('./components/HistoriqueUser.vue')['default']
     Imgautil: typeof import('./components/Imgautil.vue')['default']
     MobileBar: typeof import('./components/MobileBar.vue')['default']
---
+++ src/components/FormConnexio.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="column items-center" style="height: 10px">
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <div class="col">
+        <q-img
+          src="../assets/Logo.png"
+          spinner-color="red"
+          style="max-width: 600px"
+        />
+      </div>
+      <div class="row q-col-gutter-x-md"></div>
+      <div class="col">
+        <q-input
+          filled
+          v-model="name"
+          label="Votre email*"
+          lazy-rules
+          :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+        />
+
+        <q-input
+          filled
+          type="password"
+          label="Votre mot de passe*"
+          lazy-rules
+          :rules="[
+            (val) => (val !== null && val !== '') || 'Please type your age',
+            (val) => (val > 0 && val < 100) || 'Please type a real age',
+          ]"
+        />
+      </div>
+      <div class="col">
+        <q-btn label="Connexion" type="submit" color="primary" />
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
