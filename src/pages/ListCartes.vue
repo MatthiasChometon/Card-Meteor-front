be86f187991bce3293e0e798c7afe67d0785b74f
Ruizlouka <ruizlouka981@gmail.com>
2022-05-06 14:13:24 +0200

    feat: add formvalidation

--- src/pages/ListCartes.vue
+++ src/pages/ListCartes.vue
@@ -7,36 +7,34 @@
         <q-checkbox left-label v-model="left" label="Validée" />
       </div>
     </div>
- <div class="row">
-
-    <div class="col-1"></div>
-
-    <div class="col-2"  style="margin: 0 10px;">
-      <Imgautil style="height: 250px; max-width: 400px" />
-    </div>
-
-    <div class="col-3">
-     <DetailCarte/>
-
- </div>
-  <div class="col-1"></div>
-
-    <div class="col-2" style="margin: 0 10px;">
-      <Imgautil style="height: 250px; max-width: 400px" />
-    </div>
-
-    <div class="col-2">
-     <DetailCarte/>
-    </div>
+    <div class="row">
       <div class="col-1"></div>
 
-    <div class="col-2" style="margin: 0 10px; margin-top: 3em;">
-      <Imgautil style="height: 250px; max-width: 400px" />
-    </div>
+      <div class="col-2" style="margin: 0 10px">
+        <Imgautil style="height: 250px; max-width: 400px" />
+      </div>
 
-    <div class="col-2">
-     <DetailCarte/>
+      <div class="col-3">
+        <DetailCarte />
+      </div>
+      <div class="col-1"></div>
+
+      <div class="col-2" style="margin: 0 10px">
+        <Imgautil style="height: 250px; max-width: 400px" />
+      </div>
+
+      <div class="col-2">
+        <DetailCarte />
+      </div>
+      <div class="col-1"></div>
+
+      <div class="col-2" style="margin: 0 10px; margin-top: 3em">
+        <Imgautil style="height: 250px; max-width: 400px" />
+      </div>
+
+      <div class="col-2">
+        <DetailCarte />
+      </div>
     </div>
-  </div>
   </div>
 </template>
---
+++ src/pages/ValideCarte.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const isDisplay: bool= ref(false);
+</script>
+
+<template>
+<div>
+  <div class="row">
+    <div class="col-1"></div>
+
+    <div class="col-3">
+      <q-img
+        src="../assets/Car.png"
+        spinner-color="red"
+        style="height: 500px; max-width: 500px"
+      />
+    </div>
+
+    <div class="col-4">
+      <FormCard style="margin: 0 10px" />
+    </div>
+    </div>
+    <div class="row" style=" height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;">
+      <q-btn color="secondary">Valider</q-btn>
+      <q-btn color="amber">Modifier </q-btn>
+      <q-btn color="red">Suprimer</q-btn>
+        <q-btn label="Commenter" color="primary" @click="isDisplay = true" />
+    </div>
+  </div>
+   <q-dialog v-model="isDisplay" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Commenter</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+<q-input
+      v-model="text"
+      filled
+      type="textarea"
+      label="Commentaire"
+    />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          
+          <q-btn flat label="Commenter" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+</template>
