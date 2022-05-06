c65d14b0ae8fec634183153fdb8e5f06d21e49a7
Ruizlouka <ruizlouka981@gmail.com>
2022-05-06 17:16:22 +0200

    feat: add style

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,30 +2,36 @@
   <q-layout view="lHh Lpr lFf">
     <q-header bordered class="bg-white text-primary">
       <q-toolbar color="white">
-        <q-img
-          src="../assets/Logo.png"
-          spinner-color="red"
-          style="height: 80px; max-width: 90px"
-        />
-        <q-tabs v-model="tab">
-          <q-tab name="images"  color="#6182C3"  />
-          <q-tab name="videos"   color="#6182C3" />
-          <q-tab name="articles"   color="#6182C3" />
-          <q-tab name="images"  color="#6182C3" label="Accueil" />
-           <q-tab name="articles"   color="#6182C3" />
-          <q-tab name="videos"   color="#6182C3" label="Mes produit" />
-           <q-tab name="articles"   color="#6182C3" />
-          <q-tab name="articles"   color="#6182C3" label="Creer mon produit" />
-           <q-tab name="articles"   color="#6182C3" />
-           <q-tab name="articles"   color="#6182C3" />
-           <q-tab name="articles"   color="#6182C3" />
-        </q-tabs>
+      <img src="../assets/Logo.png" style="height: 70px"/>
         
-        <q-btn  flat color="#6182C3" >
+        <q-tabs v-model="tab">
+          <q-tab name="images" color="#6182C3" />
+          <q-tab name="videos" color="#6182C3" />
+          <q-tab name="articles" color="#6182C3" />
+          <q-tab name="images" color="#6182C3" label="Accueil" size="30px" />
+          <q-tab name="articles" color="#6182C3" />
+          <q-tab
+            name="videos"
+            color="#6182C3"
+            label="Mes produit"
+            size="30px"
+          />
+          <q-tab name="articles" color="#6182C3" />
+          <q-tab
+            name="articles"
+            color="#6182C3"
+            label="Creer mon produit"
+            size="30px"
+          />
+          <q-tab name="articles" color="#6182C3" />
+          <q-tab name="articles" color="#6182C3" />
+          <q-tab name="articles" color="#6182C3" />
+        </q-tabs>
+
+        <q-btn flat color="#6182C3" size="30px">
           <q-icon name="account_circle" class="text-purple q-gutter-md" />
-          </q-btn>
+        </q-btn>
       </q-toolbar>
     </q-header>
-
   </q-layout>
 </template>
