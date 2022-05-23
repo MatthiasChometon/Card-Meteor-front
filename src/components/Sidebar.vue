3fb2b2d35d70e427a922357922c2f9ef8f45ea25
Ruizlouka <ruizlouka981@gmail.com>
2022-05-23 08:46:22 +0200

    fix: corrected bug

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,37 +1,72 @@
 <script setup lang="ts">
-const isDisplay: boolean = ref(false)
-
+const isDisplay: boolean = ref(false);
 </script>
 <template>
   <q-header bordered class="bg-white text-primary">
     <q-toolbar color="white">
       <img src="../assets/Logo.png" style="height: 70px" />
- 
-      <q-tabs >
-        <q-tab  align="center" justify="center" />
-      <q-tab  align="center" justify="center" name="articles" color="#6182C3" />
-      <q-tab  align="center" justify="center" name="articles" color="#6182C3" />
-              <q-route-tab to="/"  align="center" justify="center" name="images" color="#6182C3" label="Accueil" size="120px" style="grid-template-columns: 1fr 1fr 1fr;" />
-        <q-tab  align="center" justify="center" name="articles" color="#6182C3" />
-        <q-tab align="center" justify="center" name="videos" color="#6182C3" label="les produits" size="30px" />
-        <q-tab align="center" justify="center" name="articles" color="#6182C3" />
-        <q-btn flat color="#6182C3" label="créer mon produit">
+      <v-spacer />
+
+      <q-tabs align="center">
+        <q-tab align="center" justify="center" right />
+        <q-spacer />
+
+        <q-route-tab
+          to="/"
+          align="center"
+          justify="center"
+          name="images"
+          color="#6182C3"
+          label="Accueil"
+          size="120px"
           
+        />
+        <q-spacer />
+        <q-tab
+          align="center"
+          justify="center"
+          name="videos"
+          color="#6182C3"
+          label="les produits"
+          size="30px"
+        />
+        <q-spacer />
+        <q-btn flat color="#6182C3" label="créer mon produit">
           <q-menu fit>
             <q-list style="min-width: 100px">
               <q-item clickable>
-                       <q-route-tab to="/CreateCard"  align="center" justify="center" name="images" color="#6182C3" label="Carte" size="120px" style="grid-template-columns: 1fr 1fr 1fr;" />
-
-              </q-item>
-               <q-separator />
-              <q-item clickable>
-                       <q-route-tab  to="/CreateDect" align="center" justify="center" name="videos" color="#6182C3" label="Dect" size="30px" />
-
+                <q-route-tab
+                  to="/CreateCard"
+                  align="center"
+                  justify="center"
+                  name="images"
+                  color="#6182C3"
+                  label="Carte"
+                  size="120px"
+                  style="grid-template-columns: 1fr 1fr 1fr"
+                />
               </q-item>
               <q-separator />
               <q-item clickable>
-                      <q-tab align="center" justify="center" name="videos" color="#6182C3" label="Box"  />
-
+                <q-route-tab
+                  to="/CreateDect"
+                  align="center"
+                  justify="center"
+                  name="videos"
+                  color="#6182C3"
+                  label="Dect"
+                  size="30px"
+                />
+              </q-item>
+              <q-separator />
+              <q-item clickable>
+                <q-tab
+                  align="center"
+                  justify="center"
+                  name="videos"
+                  color="#6182C3"
+                  label="Box"
+                />
               </q-item>
               <q-separator />
             </q-list>
