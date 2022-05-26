c586c3a54a3a012a43483a91fc3f02ab52b988e7
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-26 20:54:58 +0100

    feat: add navbar component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const links: {
+  page: string
+  text: string
+}[] = $ref([
+  {
+    page: '',
+    text: 'navbar.homepage',
+  },
+  {
+    page: '',
+    text: 'navbar.products',
+  },
+  {
+    page: '',
+    text: 'navbar.createMyProduct',
+  },
+])
+
+</script>
+
+<template>
+  <q-header elevated class="bg-white flex">
+    <div style="flex: 1;" class="q-pa-md q-pl-md">
+      <q-img
+        src="../../assets/meteor-card-icon-with-text.png" spinner-color="white"
+        style="width: 80px;" fit="contain"
+      />
+    </div>
+    <div style="flex: 3;" class="flex justify-center content-center">
+      <router-link v-for="link in links" :key="link.text" :to="link.page" replace class="text-h6 q-pa-md q-px-md">
+        {{ t(link.text) }}
+      </router-link>
+    </div>
+    <div style="flex: 1;" class="flex justify-center content-center">
+      <q-icon color="primary" size="40px" name="account_circle" class="q-px-md" />
+      <q-icon color="primary" size="40px" name="shopping_cart" class="q-px-md" />
+    </div>
+  </q-header>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+}
+</style>
