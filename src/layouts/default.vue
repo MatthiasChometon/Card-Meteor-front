00808e8006343a90377bda6610ff81d6e576dfc4
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-26 20:52:09 +0100

    feat: remove useless css

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,17 +1,9 @@
 <template>
-  <q-layout
-    style="
-      background-image: url('https://trello.com/1/cards/62849ccc3050dc1b8d203134/attachments/62849cedf6b2102e47822866/download/ng-colab-space_night_2.png');
-      background-repeat: no-repeat;
-      background-size: cover;
-    "
-  >
-    <SideBarconnected />
+  <q-layout>
+    <Navbar />
 
     <q-page-container>
-      <q-page style="padding-top: 60px" class="q-pa-md">
-        <RouterView />
-      </q-page>
+      <RouterView />
     </q-page-container>
 
     <Footer />
