9bc406aebce9eaabfe1bbdf94c654e23c4acd575
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-28 19:37:35 +0100

    feat: add ProductDirectionOrder component

---
+++ src/components/products/orderBy/ProductDirectionOrder.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useProductListFilter } from '../../../stores/products/listFilter'
+const { setDirection, getBooleanDirection } = useProductListFilter()
+const direction = $computed({
+  get() {
+    return getBooleanDirection()
+  },
+  set(newValue) {
+    setDirection(newValue)
+  },
+})
+</script>
+
+<template>
+  <q-btn v-model="direction" style="width: 60px; height: 60px;" flat class="flex justify-center items-center" color="grey" icon="arrow_upward" />
+</template>
