eec91ea3e84c8b63d3e2436afe673a831ea1679e
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-28 19:36:58 +0100

    feat: update filter and order components names

--- src/components/products/ProductFilter.vue
+++ src/components/products/ProductFilter.vue
@@ -6,10 +6,10 @@ const { isValidator, isUser } = useConnectedUser()
 <template>
   <q-card class="flex items-center shadow-4 q-my-md full-width" style="flex: 5;">
     <div class="q-pa-md flex" style="flex: 1.1; min-width: 280px;">
-      <ProductOrderDirection style="flex: 0.1;" />
-      <ProductOrderBy style="flex: 1;" />
+      <ProductDirectionOrder style="flex: 0.1;" />
+      <ProductTypeOrder style="flex: 1;" />
     </div>
-    <ProductSearch class="q-pa-md" style="flex: 1; min-width: 280px;" />
+    <ProductSearchFilter class="q-pa-md" style="flex: 1; min-width: 280px;" />
     <ProductStepFilter v-if="isValidator" class="q-pa-md" style="flex: 1;  min-width: 280px;" />
     <UserProductFilter v-if="isUser" class="q-pa-md" style="flex: 0.6;  min-width: 280px;" />
   </q-card>
