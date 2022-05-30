4a43c0ac84afeaeafaaed30377f4e07714be2c19
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-30 21:31:25 +0100

    feat: add NavbarLinks component

---
+++ src/components/layout/navbar/NavbarLinks.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { t } = useI18n()
+
+const links: {
+  page: string
+  text: string
+}[] = $ref([
+  {
+    page: '',
+    text: 'navbar.homepage',
+  },
+  {
+    page: '',
+    text: 'navbar.products',
+  },
+  {
+    page: '',
+    text: 'navbar.createMyProduct',
+  },
+])
+
+</script>
+
+<template>
+  <div>
+    <router-link
+      v-for="link in links" :key="link.text" :to="link.page" replace
+      class="text-h6 q-pa-md q-px-md text-primary flex justify-center content-center"
+    >
+      {{ t(link.text) }}
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss">
+a {
+  text-decoration: none;
+}
+</style>
