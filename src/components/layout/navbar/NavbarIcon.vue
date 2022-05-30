d3c07f08d5024bfbca1648294ba650d701b51fdc
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-30 21:31:07 +0100

    feat: create NavbarIcon component

---
+++ src/components/layout/navbar/NavbarIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <q-img
+      src="../../../assets/meteor-card-icon-with-text.png" spinner-color="white" style="width: 80px;"
+      fit="contain"
+    />
+  </div>
+</template>
