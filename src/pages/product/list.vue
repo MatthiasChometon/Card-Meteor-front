7e111e2ad5fbbfe64e0003857735eb941a24cf36
MatthiasChometon <matthiaschometon787@gmail.com>
2022-05-30 19:19:02 +0100

    feat: add CARDS_LIST_QUERY

--- src/pages/product/list.vue
+++ src/pages/product/list.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
-import type { ProductList } from '../../types/product/list/ProductList'
-const products: ProductList = []
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+import { useProductListFilter } from '~/stores/products/listFilter'
+
+const { productListFilter } = useProductListFilter()
+const CARDS_LIST_QUERY = gql`
+  query getCardsWithSearch($getCardInput: GetCardInput!) {
+    cards(getCardInput: $getCardInput) {
+      name
+    }
+  }
+`
+
+const { result, variables } = useQuery(CARDS_LIST_QUERY, {
+  getCardInput: productListFilter,
+}, { fetchPolicy: 'cache-and-network' })
+
+watch(productListFilter, () => {
+  variables.value = {
+    getCardInput: productListFilter,
+  }
+})
+
 </script>
 
 <template>
-  <ProductList :products="products" />
+  <ProductList :products="result?.cards" />
 </template>
