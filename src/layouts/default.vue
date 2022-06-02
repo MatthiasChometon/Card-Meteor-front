0d93524a26f41dae5bd294e463b2086c5f242880
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-02 21:03:26 +0100

    fix: page display

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,9 +3,20 @@
     <Navbar />
 
     <q-page-container>
-      <RouterView />
+      <RouterView class="q-py-lg" />
     </q-page-container>
 
     <Footer />
   </q-layout>
 </template>
+
+<style lang="scss">
+.q-page-container {
+  width: 100%;
+}
+.q-layout {
+  display: flex;
+}
+
+
+</style>
