cabaf26d99e321ca92488aa7f954767ca63da3f0
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-02 21:03:04 +0100

    feat: save tokens in local storage

--- src/stores/users/connected.ts
+++ src/stores/users/connected.ts
@@ -1,14 +1,17 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import { UserRoles } from '../../enums/users/UserRoles'
 import type { ConnectedUser } from '~/types/user/ConnectedUser'
+import type { LoginResponse } from '~/types/user/LoginResponse'
 
 export const useConnectedUser = defineStore('connectedUser', () => {
   let connectedUser: ConnectedUser | undefined = $ref()
   const isValidator: boolean = connectedUser?.role === UserRoles.validator
   const isUser: boolean = connectedUser?.role === UserRoles.user
 
-  function setConnectedUser(user: ConnectedUser): void {
+  function setConnectedUser({ user, accessToken, refreshToken }: LoginResponse): void {
     connectedUser = user
+    const storageToken = useStorage('tokens', { refreshToken, accessToken })
+    storageToken.value = { refreshToken, accessToken }
   }
 
   return {
