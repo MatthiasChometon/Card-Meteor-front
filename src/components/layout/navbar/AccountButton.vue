729a44667b687c276064b00f628b4f5bf24b72b7
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-02 20:59:48 +0100

    feat: add redirection to login

--- src/components/layout/navbar/AccountButton.vue
+++ src/components/layout/navbar/AccountButton.vue
@@ -1,3 +1,5 @@
 <template>
-  <q-icon color="primary" name="account_circle" class="q-px-md" />
+  <router-link to="/authentication/login">
+    <q-icon flat round color="primary" name="account_circle" class="q-px-md icon" />
+  </router-link>
 </template>
