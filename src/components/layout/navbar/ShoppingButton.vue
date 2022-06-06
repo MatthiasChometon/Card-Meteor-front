e4504d81f9c314fae14726e72e66dcda6edd670a
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-06 09:48:30 +0100

    fix: only see shoppingButton if connected

--- src/components/layout/navbar/ShoppingButton.vue
+++ src/components/layout/navbar/ShoppingButton.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import { UserRoles } from '~/enums/users/UserRoles'
+import { useConnectedUser } from '~/stores/users/connected'
+
+const { connectedUser } = useConnectedUser()
+const isUser = $computed(() => connectedUser.role === UserRoles.user)
+</script>
+
 <template>
-  <q-icon color="primary" size="40px" name="shopping_cart" class="q-px-md" />
+  <q-icon v-if="isUser" color="primary" size="40px" name="shopping_cart" class="icon" />
 </template>
