67ecdb77becd17a7c4e6995a587691139b4475b6
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-06 09:49:05 +0100

    fix: store connectedUser update

--- src/stores/users/connected.ts
+++ src/stores/users/connected.ts
@@ -1,22 +1,20 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
-import { UserRoles } from '../../enums/users/UserRoles'
 import type { ConnectedUser } from '~/types/user/ConnectedUser'
 import type { LoginResponse } from '~/types/user/LoginResponse'
 
 export const useConnectedUser = defineStore('connectedUser', () => {
-  let connectedUser: ConnectedUser | undefined = $ref()
-  const isValidator: boolean = connectedUser?.role === UserRoles.validator
-  const isUser: boolean = connectedUser?.role === UserRoles.user
+  const { $patch } = useConnectedUser()
+  const connectedUser: ConnectedUser = $ref({
+    role: 'visitor',
+  })
 
   function setConnectedUser({ user, accessToken, refreshToken }: LoginResponse): void {
-    connectedUser = user
+    $patch({ connectedUser: user })
     const storageToken = useStorage('tokens', { refreshToken, accessToken })
     storageToken.value = { refreshToken, accessToken }
   }
 
   return {
-    isValidator,
-    isUser,
     setConnectedUser,
     connectedUser,
   }
