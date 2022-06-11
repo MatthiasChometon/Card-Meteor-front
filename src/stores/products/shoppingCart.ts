385ca3e5ba8def26812001efe52f3f15555c42b5
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-11 17:59:08 +0100

    feat: add shoppingCart composable

---
+++ src/stores/products/shoppingCart.ts
@@ -0,0 +1,54 @@
+import { acceptHMRUpdate, defineStore } from 'pinia'
+import type { Product } from '~/types/product/list/Product'
+import type { PaypalPayment } from '~/types/product/shopping/PaypalPayment'
+import type { ShoppingProducts } from '~/types/product/shopping/ShoppingProducts'
+
+export const useShoppingCart = defineStore('shoppingCart', () => {
+  const shoppingCart: ShoppingProducts = $ref([])
+  const shoppingPayment: PaypalPayment = $ref({
+    email: '',
+    password: '',
+  })
+
+  function addProduct(product: Product): void {
+    const index: number = getProductIndex(product)
+    const number = (shoppingCart[index]?.number ?? 0) + 1
+    const productToAdd = { ...product, number }
+    if (index !== -1)
+      shoppingCart[index] = productToAdd
+
+    else
+      shoppingCart.push(productToAdd)
+  }
+
+  function removeProduct(product: Product): void {
+    const index: number = getProductIndex(product)
+    const hasToRemoveFromList = shoppingCart[index]?.number === 1
+    if (hasToRemoveFromList)
+      shoppingCart.splice(index, 1)
+    else
+      shoppingCart[index].number = shoppingCart[index]?.number - 1
+  }
+
+  function chooseProductNumber(product: Product, number: number): void {
+    const index: number = getProductIndex(product)
+    if (number === 0)
+      shoppingCart.splice(index, 1)
+    else shoppingCart[index].number = number
+  }
+
+  function getProductIndex(product: Product): number {
+    return shoppingCart.findIndex(shoppingProduct => shoppingProduct.id === product.id)
+  }
+
+  return {
+    shoppingCart,
+    shoppingPayment,
+    addProduct,
+    removeProduct,
+    chooseProductNumber,
+  }
+})
+
+if (import.meta.hot)
+  import.meta.hot.accept(acceptHMRUpdate(useShoppingCart, import.meta.hot))
