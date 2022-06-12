d35d21d155c72d9a8ffdead780ee2a33451cb2e5
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-12 16:49:07 +0100

    feat: change spacing

--- src/components/shopping/ShoppingDetails.vue
+++ src/components/shopping/ShoppingDetails.vue
@@ -10,7 +10,7 @@ defineEmits(['add', 'remove', 'selectNumber'])
 
 <template>
   <div class="flex">
-    <div style="flex: 2;" class="justify-center content-center flex column primary-border q-ma-md">
+    <div style="flex: 2;" class="justify-center content-center flex primary-border q-mx-md q-mt-md">
       <div class="text-primary text-h4 q-pt-md">
         {{ t('product.shopping.title') }}
       </div>
