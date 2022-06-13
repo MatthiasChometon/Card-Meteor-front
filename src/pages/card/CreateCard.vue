afc1edc5b32f2a67a30a4675f1d1291579583c50
Ruizlouka <ruizlouka981@gmail.com>
2022-06-13 10:50:14 +0200

    feat: add structure

--- src/pages/card/CreateCard.vue
+++ src/pages/card/CreateCard.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="row">
-
     <div class="col-3">
       <Imgautil style="height: 500px; max-width: 500px" />
     </div>
 
     <div class="col-4">
-      <FormCard style="margin: 0 10px;" />
+      <FormCard style="margin: 0 10px" />
     </div>
     <div class="col-4">
       <HistoriqueUser />
