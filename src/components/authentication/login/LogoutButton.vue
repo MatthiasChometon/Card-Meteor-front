86439ae046f03d2276cc148b3c2f64795f84663c
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-16 22:51:08 +0100

    feat: add logout component

---
+++ src/components/authentication/login/LogoutButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { t } = useI18n()
+defineEmits(['clickLogout'])
+</script>
+
+<template>
+  <q-btn color="primary" @click="$emit('clickLogout')">
+    {{ t('account.logout') }}
+  </q-btn>
+</template>
