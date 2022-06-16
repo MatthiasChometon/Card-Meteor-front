e52c0fc622d9fb3380ca5b40ed5e3666bad7f939
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-16 22:52:56 +0100

    feat: add BurgerMenuButton and BurgerMenuLinks components

---
+++ src/components/layout/BurgerMenuButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useBurgerMenuButton } from '~/stores/navbar/burgerMenu'
+const isOpen = useBurgerMenuButton()
+</script>
+
+<template>
+  <q-icon color="primary" size="60px" name="menu" class="q-pa-md mobile-navbar home-icon" @click="isOpen = !isOpen" />
+  <q-drawer v-model="isOpen" side="left" overlay behavior="mobile" elevated>
+    <div class="flex q-pa-md justify-end">
+      <CloseDrawer @click-close-drawer="isOpen = !isOpen" />
+    </div>
+    <div class="flex column content-center">
+      <BurgerMenuLinks />
+    </div>
+    <div class="flex q-pa-lg q-ma-lg justify-center">
+      <LogoutButton @click-logout="isOpen = !isOpen" />
+    </div>
+  </q-drawer>
+</template>
