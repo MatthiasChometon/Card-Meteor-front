21a18d300b3fc3562d74ed9bb7001b9ee72bb3b1
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-16 17:53:20 +0100

    feat: add Account type

---
+++ src/types/user/Account.ts
@@ -0,0 +1,10 @@
+import type { Graphql } from '../graphql/Graphql'
+
+export interface Account extends Graphql {
+  lastName: string
+  firstName: string
+  username: string
+  email: string
+  phone: string
+  password: string
+}
