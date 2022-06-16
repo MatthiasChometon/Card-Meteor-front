632f1cabc6808ab611e182e227dc20bca8e86744
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-16 17:52:41 +0100

    feat: delete sugar ref from AccountButton component

--- src/components/layout/navbar/AccountButton.vue
+++ src/components/layout/navbar/AccountButton.vue
@@ -3,9 +3,9 @@ import { UserRoles } from '~/enums/users/UserRoles'
 import { useConnectedUser } from '~/stores/users/connected'
 
 const { connectedUser } = useConnectedUser()
-const isUser = $computed(() => connectedUser.role === UserRoles.user)
-const firstNameLetter = $computed(() => connectedUser?.firstName?.charAt(0)?.toUpperCase())
-const lastNameLetter = $computed(() => connectedUser?.lastName?.charAt(0)?.toUpperCase())
+const isUser = computed(() => connectedUser.role === UserRoles.user)
+const firstNameLetter = computed(() => connectedUser?.firstName?.charAt(0)?.toUpperCase())
+const lastNameLetter = computed(() => connectedUser?.lastName?.charAt(0)?.toUpperCase())
 </script>
 
 <template>
