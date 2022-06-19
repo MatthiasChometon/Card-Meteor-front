99f28f8800192afaa05eb4bdb7af2062b4776871
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-19 18:18:16 +0100

    feat: add CardCreation components

---
+++ src/components/card/CardCreationForm.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { useInCreationCard } from '~/stores/card/create'
+const { t } = useI18n()
+const inCreationCard = useInCreationCard()
+</script>
+
+<template>
+  <div class="flex column no-wrap">
+    <Title traduction-text="card.create.title" />
+    <q-form class="flex">
+      <div>
+        <q-input
+          v-model="inCreationCard.name" class="q-pa-sm" outlined :label="t('card.create.name')"
+          :rules="[val => val && val.length > 0 || '']" :maxlength="32"
+        />
+        <CardAttributeInput />
+        <CardRaceInput />
+        <CardLevelInput />
+        <q-input
+          v-model="inCreationCard.attack" class="q-pa-sm" outlined :label="t('card.create.attack')"
+          :rules="[val => val && val.length > 0 || '']" :maxlength="4"
+        />
+        <q-input
+          v-model="inCreationCard.defense" class="q-pa-sm" outlined :label="t('card.create.defense')"
+          :rules="[val => val && val.length > 0 || '']" :maxlength="4"
+        />
+        <q-toggle v-model="inCreationCard.hasEffect" class="q-pa-sm" :label="t('card.create.hasEffect')" />
+        <q-input
+          v-model="inCreationCard.description" class="q-pa-sm" outlined autogrow
+          :label="t('card.create.description')"
+        />
+      </div>
+
+      <div>
+        <q-input
+          v-model="inCreationCard.archetype" class="q-pa-sm" outlined :label="t('card.create.archetype')"
+          :maxlength="30"
+        />
+        <q-input
+          v-model="inCreationCard.backgroundPicture" class="q-pa-sm" outlined
+          :label="t('card.create.backgroundPicture')" :maxlength="500"
+        />
+        <CardTypeInput />
+        <CardLimitationInput />
+        <q-input
+          v-model="inCreationCard.serialNumber" class="q-pa-sm" outlined :label="t('card.create.serialNumber')"
+          :maxlength="30"
+        />
+        <q-input
+          v-model="inCreationCard.edition" class="q-pa-sm" outlined :label="t('card.create.edition')"
+          :rules="[val => val && val.length > 0 || '']" :maxlength="30"
+        />
+        <q-input
+          v-model="inCreationCard.editor" class="q-pa-sm" outlined :label="t('card.create.editor')"
+          :rules="[val => val && val.length > 0 || '']" :maxlength="30"
+        />
+      </div>
+    </q-form>
+  </div>
+</template>
---
+++ src/components/card/CardCreationRendering.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useInCreationCard } from '~/stores/card/create'
+
+const inCreationCard = useInCreationCard()
+const typeForPicture = computed(() => {
+  const emptyMonsterType = inCreationCard.value.hasEffect ? 'effect' : 'normal'
+  return inCreationCard.value.type === ''
+    ? emptyMonsterType
+    : inCreationCard.value.type
+})
+const backgroundPicture = computed(() => new URL(`../../assets/card-templates/${typeForPicture.value}.png`, import.meta.url).href)
+
+</script>
+
+<template>
+  <q-img :src="backgroundPicture" class="card-creation-rendering-picture">
+    <div
+      style="background: transparent !important; height: 100%; padding: 0px !important; width: 100%;"
+      class="flex column"
+    >
+      <CardTitleRendering />
+      <CardStarsRendering />
+      <CardCoverPictureRendering />
+      <CardEditionRendering />
+      <CardDescriptionRendering />
+      <CardIdsRendering />
+    </div>
+  </q-img>
+</template>
+
+<style scoped lang="scss">
+@media (min-width: $breakpoint-lg-min) {
+  .card-creation-rendering-picture {
+    height: 520px;
+    width: 356.8px;
+  }
+}
+
+@media (max-width: $breakpoint-md-max) {
+  .card-creation-rendering-picture {
+    height: 455px;
+    width: 312.2px;
+  }
+}
+
+@media (max-width: $breakpoint-sm-max) {
+  .card-creation-rendering-picture {
+    height: 422.5px;
+    width: 289.9px;
+  }
+}
+
+@media (max-width: $breakpoint-xs-max) {
+  .card-creation-rendering-picture {
+    height: 325px;
+    width: 223px;
+  }
+}
+</style>
