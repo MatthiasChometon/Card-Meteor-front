d7763b87c81e4772456bffd91dbb7670b54e1cdb
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-19 18:18:31 +0100

    feat: add CardComment components

---
+++ src/components/card/comment/CardComment.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="primary-border q-pa-sm q-ma-md">
+    <div class="flex justify-between">
+      <CardCommentValid :is-valid="true" />
+      <CardCommentDate />
+    </div>
+    <div class="flex justify-center q-pa-sm text-secondary">
+      "plus d'attaques"
+    </div>
+  </div>
+</template>
