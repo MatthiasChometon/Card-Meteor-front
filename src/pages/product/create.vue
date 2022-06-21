bb291cdae9bce7e9296259fe846618cb6807a49e
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-21 02:38:23 +0100

    feat: add pictures to creation of card

--- src/pages/product/create.vue
+++ src/pages/product/create.vue
@@ -3,15 +3,23 @@ import { useMutation } from '@vue/apollo-composable'
 import { useNotification } from '../../stores/notification'
 import { CREATE_CARD } from '~/graphql/card/create'
 import { useInCreationCard } from '~/stores/card/create'
+import { useHtmlToPicture } from '~/composables/useHtmlToPicture'
 
 const { sendNotification } = useNotification()
 const inCreationCard = useInCreationCard()
+const { getPicturePath, picture: cardPicture } = useHtmlToPicture('cardCreation')
 const { mutate: createCard, onDone } = useMutation(CREATE_CARD, {
   fetchPolicy: 'network-only',
 })
-function create() {
-  const { __typename, ...newCard } = inCreationCard.value
-  createCard({ newCard })
+async function create() {
+  await getPicturePath()
+  const { __typename, backgroundPicture, coverPicture, ...newCardWithoutPictures } = inCreationCard.value
+  const newCard = { ...newCardWithoutPictures }
+  createCard({
+    newCard,
+    coverPicture: cardPicture.value,
+    backgroundPicture: inCreationCard.value.backgroundPicture,
+  })
 }
 onDone((result) => {
   sendNotification(result, { path: 'register.onSuccess' }, { path: 'register.onError' })
