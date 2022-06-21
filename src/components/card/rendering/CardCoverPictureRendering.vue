8c41adbfb0ce27d80e23a7c509e57d9a779c6c22
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-21 02:39:57 +0100

    feat: display backgroundPicture

--- src/components/card/rendering/CardCoverPictureRendering.vue
+++ src/components/card/rendering/CardCoverPictureRendering.vue
@@ -2,11 +2,17 @@
 import { useInCreationCard } from '~/stores/card/create'
 
 const inCreationCard = useInCreationCard()
-const coverPicture = computed(() => new URL(inCreationCard.value.backgroundPicture, import.meta.url).href)
+const coverPicture = computed(() => {
+  const backgroundPicture = inCreationCard.value.backgroundPicture
+  const hasFile = backgroundPicture instanceof File
+  if (backgroundPicture === undefined || !hasFile)
+    return
+  return URL.createObjectURL(backgroundPicture)
+})
 </script>
 
 <template>
   <div style="height: 55%; width: 100%; padding: 2%;" class="flex justify-center">
-    <q-img :src="coverPicture" fit="contain" style="width: 79.8%; margin-left: 0.5%;" ratio="1" />
+    <q-img :src="coverPicture" fit="cover" style="width: 79.8%; margin-left: 0.5%;" ratio="1" />
   </div>
 </template>
