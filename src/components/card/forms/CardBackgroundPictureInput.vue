a5a2ddc07ee7fefa9361528b5b167b1df1fe5531
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-23 20:08:55 +0100

    feat: add CardBackgroundPictureInput component

---
+++ src/components/card/forms/CardBackgroundPictureInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import { useInCreationCard } from '~/stores/card/create'
+const { t } = useI18n()
+const inCreationCard = useInCreationCard()
+const backgroundPicture: Ref<null | File> = ref(null)
+watch(backgroundPicture, () => {
+  const isFile = backgroundPicture.value instanceof File
+  if (!isFile)
+    return
+  inCreationCard.value.backgroundPicture = backgroundPicture.value
+})
+</script>
+
+<template>
+  <q-file v-model="backgroundPicture" class="q-pa-sm" outlined :label="t('card.create.backgroundPicture')">
+    <template #prepend>
+      <q-icon name="attach_file" />
+    </template>
+  </q-file>
+</template>
