eff791a8014a0c1a47e11aea5d4a16bcc19e6678
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-23 20:04:34 +0100

    feat: delete useCardCoverPicture composable

--- src/composables/useCardCoverPicture.ts
+++
@@ -1,8 +0,0 @@
-import type { ComputedRef } from 'vue'
-
-export function useCardCoverPicture(coverPicture: string): { picture: ComputedRef<string> } {
-  const serverUrl = import.meta.env.VITE_GRAPHQL_URL
-  const picture = computed(() => `${serverUrl}/cover/${coverPicture}`)
-
-  return { picture }
-}
