4cbc8dcb73c6ec15215e479356a2eca64a6edf08
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-26 00:06:48 +0100

    refactor: create ProductCardEditButton component

---
+++ src/components/products/card/ProductCardEditButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  productId: string
+}>()
+</script>
+
+<template>
+  <router-link :to="`/card/${props.productId}`">
+    <q-btn color="primary" icon="edit" round outline />
+  </router-link>
+</template>
