1fb0b38ff6cb86f77ec9e13af73fd5a6e4564cf0
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-25 13:31:42 +0100

    feat: create links for user and visitor in store

---
+++ src/stores/links/user.ts
@@ -0,0 +1,29 @@
+import type { CreateGlobalStateReturn, RemovableRef } from '@vueuse/core'
+import { createGlobalState } from '@vueuse/core'
+import { useProductListFilter } from '../products/listFilter'
+import type { Link } from '~/types/Link'
+
+const userLinks: Link[] = [
+  {
+    page: '/user/account',
+    text: 'links.account',
+  },
+  {
+    page: '/user/order',
+    text: 'links.order',
+  },
+  {
+    page: '/product/list',
+    text: 'links.products',
+    event: () => setEditor(),
+  },
+]
+
+function setEditor() {
+  const { setEditor } = useProductListFilter()
+  setEditor(true)
+}
+
+export const useUserLinks: CreateGlobalStateReturn<RemovableRef<Link[]>> = createGlobalState(
+  () => ref(userLinks),
+)
