01738c0fd38dcb8374cf4a1c53281c1b2776f9a4
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-27 23:20:44 +0100

    feat: add CardCommmentForm component

---
+++ src/components/card/comment/CardCommentForm.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const { t } = useI18n()
+const emit = defineEmits(['validate', 'sendComment'])
+
+const newComment = ref('')
+const isCardValidated = ref(false)
+
+const isValidatedTranslation = computed(() => {
+  const isValidatedKey = isCardValidated.value ? 'validated' : 'notValidated'
+  return t(`card.comment.form.${isValidatedKey}`)
+})
+const buttonText = computed(() => {
+  const isValidatedKey = isCardValidated.value ? 'validate' : 'sendComment'
+  return t(`card.comment.form.${isValidatedKey}`)
+})
+
+function click() {
+  isCardValidated.value ? emit('validate') : emit('sendComment', newComment.value)
+}
+</script>
+
+<template>
+  <Title traduction-text="card.comment.form.title" />
+  <div class="flex justify-center content-center q-pb-md">
+    <q-checkbox v-model="isCardValidated" color="primary" />
+    <div class="flex justify-center content-center">
+      {{ isValidatedTranslation }}
+    </div>
+  </div>
+  <q-input
+    v-if="!isCardValidated" v-model="newComment" outlined color="primary"
+    :label="t('card.comment.form.comment')" type="textarea" autogrow
+  />
+  <q-btn style="min-width: 240px;" class="q-my-md" color="primary" @click="click">
+    {{ buttonText }}
+  </q-btn>
+</template>
