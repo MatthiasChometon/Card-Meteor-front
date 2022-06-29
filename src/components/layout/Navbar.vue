20b8b11da5c4e0ff8dc7c273754e73cea7e233d0
MatthiasChometon <matthiaschometon787@gmail.com>
2022-06-29 21:57:02 +0100

    feat: use DesktopNavbar and MobileNavbar components inside Navbar component

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,43 +1,11 @@
 <template>
   <q-header class="bg-white flex q-header">
-    <NavbarIcon style="flex: 1;" class="q-pa-md q-pl-md desktop-navbar" />
-    <NavbarLinks style="flex: 2;" class="justify-center content-center desktop-navbar" />
-    <NavbarOptions style="flex: 1;" class="justify-end items-center no-wrap desktop-navbar" />
-
-    <div class="mobile-navbar" style="flex: 1;">
-      <BurgerMenuButton />
-    </div>
-    <NavbarIcon style="flex: 3;" class="mobile-navbar q-pa-md" />
-    <div class="mobile-navbar">
-      <AuthenticationButton class="q-pr-md" />
-      <ShoppingButton class="q-px-md" />
-    </div>
+    <DesktopNavbar style="flex: 1;" />
+    <MobileNavbar style="flex: 1;" />
   </q-header>
 </template>
 
 <style lang="scss">
-@media (min-width: $breakpoint-sm-min) {
-  .mobile-navbar {
-    display: none;
-  }
-
-  .desktop-navbar {
-    display: flex;
-  }
-}
-
-@media (max-width: $breakpoint-sm-max) {
-  .mobile-navbar {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .desktop-navbar {
-    display: none;
-  }
-}
-
 body.body--dark .q-header,
 body.body--dark .q-footer,
 body.body--dark .q-drawer {
