7ca816a3a157d0b1d42c73faa5311d6e3154c0d0
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-01 03:39:25 +0200

    feat: add OrderDetailsProduct component

---
+++ src/components/orders/OrderDetailsProduct.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { OrderProduct } from '~/types/order/OrderProduct'
+
+const props = defineProps<{
+  orderProduct: OrderProduct
+}>()
+</script>
+
+<template>
+  <div class="row q-pa-sm">
+    <q-badge> {{ props.orderProduct.number }}</q-badge>
+    <div class="q-ml-xl">
+      {{ props.orderProduct.product.name }}
+    </div>
+  </div>
+</template>
