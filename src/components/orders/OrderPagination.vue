449c96260509a1cf52f8a04591bdb382485ec871
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-01 03:39:08 +0200

    feat: add OrderPagination component

---
+++ src/components/orders/OrderPagination.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const currentPage = ref(1)
+const emit = defineEmits(['setNewPagination'])
+watch(currentPage, () => emit('setNewPagination', currentPage.value))
+</script>
+
+<template>
+  <q-card class="q-py-xs flex justify-center">
+    <q-pagination
+      v-model="currentPage"
+      :max="60"
+      :max-pages="8"
+      :boundary-numbers="false"
+      direction-links
+      flat
+      color="grey-4"
+      active-color="primary"
+    />
+  </q-card>
+</template>
