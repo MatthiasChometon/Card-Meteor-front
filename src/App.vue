05c390474039d5ff1f7c72c88e6037551c024187
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-02 17:41:13 +0200

    style: add valid import for useRefreshConnectedUser

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
-<script setup lang="ts">import { useRefreshConnectedUser } from './composables/useRefreshConnectedUser'
+<script setup lang="ts">
+import { useRefreshConnectedUser } from './composables/useRefreshConnectedUser'
 
 useHead({
   title: 'Meteor Card',
