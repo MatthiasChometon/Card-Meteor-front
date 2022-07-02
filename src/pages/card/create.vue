ae4f3684b459e2d3acf3b0fb416d6e61441fe280
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-02 14:24:12 +0200

    feat: redirect when create card if not connected

--- src/pages/card/create.vue
+++ src/pages/card/create.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import { useMutation } from '@vue/apollo-composable'
+import { useQuasar } from 'quasar'
 import { useNotification } from '../../stores/notification'
 import { CREATE_CARD } from '~/graphql/card/create'
 import { useInCreationCard } from '~/stores/card/create'
 import { useHtmlToPicture } from '~/composables/useHtmlToPicture'
+import { useUserRole } from '~/composables/useUserRole'
 
+const { isVisitor, isUser } = useUserRole()
+const { t } = useI18n()
+const $q = useQuasar()
 const { sendNotification } = useNotification()
 const router = useRouter()
 const inCreationCard = useInCreationCard()
@@ -12,6 +17,18 @@ const { getPicturePath, picture: cardPicture } = useHtmlToPicture('cardCreation'
 const { mutate: createCard, onDone } = useMutation(CREATE_CARD, {
   fetchPolicy: 'network-only',
 })
+
+function validateCard() {
+  if (isVisitor.value) {
+    const message = t('card.create.notConnected')
+    $q.notify({ message, color: 'info' })
+    router.replace('/authentication/register')
+  }
+
+  if (isUser.value)
+    create()
+}
+
 async function create() {
   await getPicturePath()
   const { __typename, backgroundPicture, coverPicture, ...newCardWithoutPictures } = inCreationCard.value
@@ -31,5 +48,5 @@ defineEmits(['clickValidateCard'])
 </script>
 
 <template>
-  <CardCreation @click-validate-card="create" />
+  <CardCreation @click-validate-card="validateCard" />
 </template>
