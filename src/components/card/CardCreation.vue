4a96d4530398026d7f4704f1835d032c95f15607
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-03 20:18:49 +0200

    feat: add responsive for mobile version of CardCreation component

--- src/components/card/CardCreation.vue
+++ src/components/card/CardCreation.vue
@@ -7,12 +7,22 @@ defineEmits(['clickValidateCard'])
     <div style="flex: 1;" class="justify-center flex content-center">
       <CardCreationRendering />
     </div>
-    <div style="flex: 1;" class="justify-center flex content-center">
+    <div style="flex: 1;" class="justify-center flex content-center card-creation-form-container q-py-md">
       <CardCreationForm />
     </div>
-    <div style="flex: 1;" class="justify-center flex content-center column">
+    <div style="flex: 1; min-width: 270px;" class="justify-center flex content-center column q-py-md">
       <CardCreationComments />
       <CardCreationValidateButton @click-validate-card="$emit('clickValidateCard')" />
     </div>
   </div>
 </template>
+
+<style scoped lang="scss">
+@media (min-width: $breakpoint-sm-min) {
+  .card-creation-form-container { min-width: 450px; }
+}
+
+@media (max-width: $breakpoint-xs-max) {
+  .card-creation-form-container { min-width: 400px; }
+}
+</style>
