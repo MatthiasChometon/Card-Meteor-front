099c19fdd4819de6cbc89ea4c4f5f7b4702a6021
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-04 23:42:32 +0200

    feat: add DownloadAppButton component

---
+++ src/components/layout/DownloadAppButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useDownloadMobileApp } from '~/composables/useDownloadMobileApp'
+
+const { download } = useDownloadMobileApp()
+</script>
+
+<template>
+  <q-icon name="file_download" class="icon home-icon" color="primary" @click="download" />
+</template>
