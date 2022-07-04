76ab6595f9aac53ccaece45071a5faa7329c00dd
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-04 23:42:58 +0200

    feat: add DownloadAppTextButton component to BurgerMenuLinks component

--- src/components/layout/BurgerMenuLinks.vue
+++ src/components/layout/BurgerMenuLinks.vue
@@ -8,8 +8,9 @@ const { allLinks } = useLinks()
     v-for="link in allLinks" :key="link.text" :to="link.page" :link="link"
     class="text-h6 q-pa-md flex text-primary justify-center"
   />
-  <div class="flex q-pt-md">
+  <div class="flex q-py-md">
     <SwitchDarkMode />
     <SwitchLanguageButton />
   </div>
+  <DownloadAppTextButton class="flex q-mt-md" />
 </template>
