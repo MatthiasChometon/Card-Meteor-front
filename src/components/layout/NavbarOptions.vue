dec6effde24a746445c9377f308502e3bd41021d
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-04 23:41:59 +0200

    feat: add DownloadAppButton component to NavbarOptions component

--- src/components/layout/NavbarOptions.vue
+++ src/components/layout/NavbarOptions.vue
@@ -2,6 +2,7 @@
   <div>
     <SwitchDarkMode class="q-mr-md" />
     <SwitchLanguageButton class="q-mr-md" />
+    <DownloadAppButton style="padding-top: 2px;" />
     <AuthenticationButton class="q-mr-md" />
     <AccountButton class="q-mr-md" />
     <ShoppingButton class="q-mr-md" />
