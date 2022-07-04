196227a783c1ce714c874a8816c990fdb684a59d
MatthiasChometon <matthiaschometon787@gmail.com>
2022-07-04 23:42:18 +0200

    feat: add DownloadAppTextButton component

---
+++ src/components/layout/DownloadAppTextButton.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useDownloadMobileApp } from '~/composables/useDownloadMobileApp'
+
+const { t } = useI18n()
+const { download } = useDownloadMobileApp()
+</script>
+
+<template>
+  <q-btn :label="t('downloadAppButton')" color="primary" @click="download" />
+</template>
